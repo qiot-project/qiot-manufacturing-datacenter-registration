de8ad9072ba1fe5d1f30dcc7057300a00b834117
Andrea Battaglia <andreabattaglia@users.noreply.github.com>
2021-09-18 12:46:26 +0200

    1.0.0.alpha 7 release

--- build.sh
+++ build.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 
-echo "Datacenter - Registration"
 cd ~/git/qiot/MANUFACTORING/qiot-manufacturing-datacenter-registration
-./mvnw package -U -Pnative -Dquarkus.native.container-build=true
+./mvnw clean package -U -Pnative -Dquarkus.native.container-build=true
 docker rmi quay.io/qiotmanufacturing/datacenter-registration:1.0.0-alpha7 --force
 docker build -f src/main/docker/Dockerfile.native -t quay.io/qiotmanufacturing/datacenter-registration:1.0.0-alpha7 .
 docker push quay.io/qiotmanufacturing/datacenter-registration:1.0.0-alpha7
