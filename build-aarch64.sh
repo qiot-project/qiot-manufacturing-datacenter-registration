d82a6e6c9a6121e87ddb533194baff453312064b
Andrea Battaglia <andreabattaglia@users.noreply.github.com>
2021-11-10 01:41:40 +0100

    fix placeholders

--- build-aarch64.sh
+++ build-aarch64.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-docker login quay.io -u ${QUAY_USERNAME} -p ${QUAY_PASSWORD}
+docker login quay.io -u ${QUAY_ALL_USERNAME} -p ${QUAY_ALL_PASSWORD}
 PV=$(mvn org.apache.maven.plugins:maven-help-plugin:3.2.0:evaluate -Dexpression=project.version -q -DforceStdout)-aarch64
 docker run --rm --privileged multiarch/qemu-user-static:register --reset
 docker rmi quay.io/qiotproject/all-registration:$PV --force
--- build.sh
+++ build.sh
@@ -3,5 +3,5 @@
 mvn -B clean package -Pprod,native oc:build oc:push \
           -Dquarkus.native.container-build=true \
           -Dquarkus.container-image.build=true \
-          -Djkube.docker.push.username=${QUAY_USERNAME} \
-          -Djkube.docker.push.password=${QUAY_PASSWORD} 
+          -Djkube.docker.push.username=${QUAY_ALL_USERNAME} \
+          -Djkube.docker.push.password=${QUAY_ALL_PASSWORD} 
